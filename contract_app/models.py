d7be5a1ea94ef5ec192b7d2a9391c8f512b330a4
cp_shen <cp_shen@outlook.com>
2018-12-18 00:55:44 +0800

    add some validations

--- contract_app/models.py
+++ contract_app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.exceptions import ValidationError
 from django.contrib.auth.models import (
     BaseUserManager,
     AbstractBaseUser,
@@ -6,12 +7,19 @@ from django.contrib.auth.models import (
 
 
 class MyUserManager(BaseUserManager):
-    def create_user(self, email, password):
+    def create_user(self, email, username, password):
         if not email:
-            raise ValueError('no email provided')
+            raise ValueError('Users must have an email address')
+        if not username:
+            raise ValueError('no username provided')
         if not password:
             raise ValueError('no password provided')
-        user = self.model(email=self.normalize_email(email))
+
+        user = self.model(
+            email=self.normalize_email(email),
+            username=username
+        )
+
         user.set_password(password)
         user.save(using=self._db)
         return user
@@ -19,6 +27,7 @@ class MyUserManager(BaseUserManager):
 
 class MyUser(AbstractBaseUser):
     email = models.EmailField(max_length=255, unique=True)
+    username = models.EmailField(max_length=255, unique=True)
 
     objects = MyUserManager()
 
@@ -58,29 +67,62 @@ class Contract(models.Model):
     status = models.CharField(choices=STATUS_CHOICES, default=CREATED, max_length=50)
     # attachment with 10 MB max size
     # attachment = models.BinaryField(null=True, editable=True, max_length=10 * 1024 * 1024)
-    # todo: add model data validation in clean and save,
-    # such as procudure control, and date comparison
+
+    author = models.ForeignKey(MyUser, on_delete=models.CASCADE, editable=False)
+    users_countersign = models.ManyToManyField(MyUser, through='Countersign', related_name='contracts_countersign')
+    users_review = models.ManyToManyField(MyUser, through='Review', related_name='contracts_review')
+    users_sign = models.ManyToManyField(MyUser, through='Sign', related_name='contracts_sign')
 
     def __str__(self):
         return ':'.join([str(self.id), self.title])
 
+    def check_countersign(self):
+        count = self.countersign_set.filter(is_confirmed=False).count()
+        if count > 0:
+            raise ValidationError('%s people have not countersigned' % count)
+
+    def check_review(self):
+        count = self.review_set.filter(is_confirmed=False).count()
+        if count > 0:
+            raise ValidationError('%s people have not reviewed' % count)
+
+    def check_sign(self):
+        count = self.sign_set.filter(is_confirmed=False).count()
+        if count > 0:
+            raise ValidationError('%s people have not signed' % count)
+
+    def clean(self):
+        if self.status == self.CREATED:
+            pass
+        elif self.status == self.COUNTERSIGNED:
+            self.check_countersign()
+        elif self.status == self.REWRITED:
+            self.check_countersign()
+        elif self.status == self.REVIEWED:
+            self.check_countersign()
+            self.check_review()
+        elif self.status == self.SIGNED:
+            self.check_countersign()
+            self.check_review()
+            self.check_sign()
+
 
 class Countersign(models.Model):
-    user = models.ForeignKey(MyUser, on_delete=models.CASCADE)
-    contract = models.ForeignKey(Contract, on_delete=models.CASCADE)
+    user = models.ForeignKey(MyUser, on_delete=models.CASCADE, editable=False)
+    contract = models.ForeignKey(Contract, on_delete=models.CASCADE, editable=False)
     message = models.CharField(max_length=1000)
     is_confirmed = models.BooleanField(default=False)
 
 
 class Review(models.Model):
-    user = models.ForeignKey(MyUser, on_delete=models.CASCADE)
-    contract = models.ForeignKey(Contract, on_delete=models.CASCADE)
+    user = models.ForeignKey(MyUser, on_delete=models.CASCADE, editable=False)
+    contract = models.ForeignKey(Contract, on_delete=models.CASCADE, editable=False)
     message = models.CharField(max_length=1000)
     is_confirmed = models.BooleanField(default=False)
 
 
 class Sign(models.Model):
-    user = models.ForeignKey(MyUser, on_delete=models.CASCADE)
-    contract = models.ForeignKey(Contract, on_delete=models.CASCADE)
+    user = models.ForeignKey(MyUser, on_delete=models.CASCADE, editable=False)
+    contract = models.ForeignKey(Contract, on_delete=models.CASCADE, editable=False)
     message = models.CharField(max_length=1000)
     is_confirmed = models.BooleanField(default=False)
--- contract_app/serializers.py
+++ contract_app/serializers.py
@@ -3,12 +3,22 @@ from .models import Contract, Client
 
 
 class ContractSerializer(serializers.ModelSerializer):
+    author = serializers.SlugRelatedField(
+        read_only=True,
+        slug_field='username'
+    )
+
     class Meta:
         model = Contract
         fields = (
             'id', 'title', 'date_begin', 'date_end', 'content',
-            'clients', 'status',
+            'clients', 'status', 'author',
         )
+
+    def validate(self, data):
+        if data['date_begin'] > data['date_end']:
+            raise serializers.ValidationError("contract finish must occur after start")
+        return data
 
 
 class ClientSerializer(serializers.ModelSerializer):
